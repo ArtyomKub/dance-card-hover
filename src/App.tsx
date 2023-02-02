31eba9998e1a7188bfdea271b0b1c78b7cbfd2b4
ArtyomKub <kub.killjoy@gmail.com>
2023-02-02 23:36:16 +0300

    I have any error

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='cardhover'>
+            <div className='cardhover__content card-1'>
+                HOVER ME ANYWHERE
+            </div>
+        </div>
+    );
 }
 
 export default App;
